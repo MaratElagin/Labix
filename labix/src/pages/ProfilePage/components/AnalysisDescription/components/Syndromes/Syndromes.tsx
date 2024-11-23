17bcf8096d19e6fc3824728a1db730f180fb965a
Marat Elagin <elagin@mindbox.cloud>
2024-11-23 14:53:58 +0300

    frontend: add tooltips

--- labix/src/pages/ProfilePage/components/AnalysisDescription/components/Syndromes/Syndromes.tsx
+++ labix/src/pages/ProfilePage/components/AnalysisDescription/components/Syndromes/Syndromes.tsx
@@ -6,6 +6,8 @@ import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
 import TableBody from "@mui/material/TableBody";
 import Paper from "@mui/material/Paper";
+import {IconButton, Tooltip} from "@mui/material";
+import {ReactComponent as Icon} from '../../../../../../svg/info.svg';
 
 interface SyndromesProps {
     syndromes: Array<{
@@ -26,6 +28,14 @@ export const Syndromes: React.FC<SyndromesProps> = ({syndromes}) => {
                         <TableRow>
                             <TableCell sx={{fontWeight: 'bold'}}>Система</TableCell>
                             <TableCell sx={{fontWeight: 'bold'}}>Проблемы</TableCell>
+                            <TableCell sx={{ fontWeight: 'bold' }}>
+                                Рекомендации
+                                <Tooltip title="Эти рекомендации не являются окончательными, проконсультируйтесь с врачом.">
+                                    <IconButton>
+                                        <Icon className="icon"/>
+                                    </IconButton>
+                                </Tooltip>
+                            </TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
@@ -36,6 +46,9 @@ export const Syndromes: React.FC<SyndromesProps> = ({syndromes}) => {
                                     {syndrome.problems.map((problem, i) => (
                                         <div key={i}>{problem}</div>
                                     ))}
+                                </TableCell>
+                                <TableCell>
+                                    Сдать общий биохимический анализ крови
                                 </TableCell>
                             </TableRow>
                         ))}
